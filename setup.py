fdda83498e0b56dd89f2fa9232e130778ad06913
Brent M. Spell <bspell1@gmail.com>
2018-07-02 09:59:58 -0400

    fix cython on linux

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@
 import setuptools
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
 
 setup(name='streamp3',
-      version='0.1.1',
+      version='0.1.5',
       description="streaming mp3 decoder",
       long_description=open('README.md', 'r').read(),
       long_description_content_type='text/markdown',
@@ -14,10 +13,10 @@ setup(name='streamp3',
       author="Brent M. Spell",
       author_email='brent@pylon.com',
       packages=setuptools.find_packages(),
-      setup_requires=['cython'],
-      ext_modules=cythonize([Extension('lame.hip',
-                                       ['lame/hip.pyx'],
-                                       libraries=['mp3lame'])]),
+      setup_requires=['Cython>=0.28.3'],
+      ext_modules=[Extension('lame.hip',
+                             ['lame/hip.pyx'],
+                             libraries=['mp3lame'])],
       classifiers=("Programming Language :: Python :: 3",
                    "License :: OSI Approved :: Apache Software License",
                    "Operating System :: OS Independent"))
